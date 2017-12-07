7b8ece0e97362c39ce31ef3151d2df27175888bd
Alisher Gani <misteralishergani@mail.ru>
2017-12-07 23:13:16 +0600

    Add files via upload

---
+++ vue/src/views/Users.vue
@@ -0,0 +1,25 @@
+<template>
+	<h1 class="users">Users.vue</h1>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.users {
+		width: 100%;
+		height: 100vh;
+		color: #fff;
+		background-color: blue;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
